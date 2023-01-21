ce8ae3476fd56a47e3bed019e89af19f3b70324f
Ivo <ivopr@proton.me>
2023-01-21 17:29:38 -0400

    aula 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,13 @@
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
-app.register(cors)
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(cors);
+app.register(appRoutes);
 
 app.listen({
-  host: "0.0.0.0",
-  port: 3333
-}).then(() => console.log("App running"))
+	host: "0.0.0.0",
+	port: 3333
+}).then(() => console.log("App running")).catch(console.error)
